17fec1602aed3898d3c71fe59ceba6760e9b1e13
nurgalive <github@nurgalive.com>
2024-06-06 20:27:47 +0200

    Added tests for the tyme.py

--- nurgapy/tests/tyme_test.py
+++ nurgapy/tests/tyme_test.py
@@ -1,9 +1,23 @@
 from nurgapy import tyme
+import time
 
 
-def func():
-    pass
+def sleep_func(n):
+    """
+    Function used for testing.
+    """
+    time.sleep(n)
+    return n
 
 
-def test_tyme():
-    tyme(func)
+# Asserting the function output
+# https://docs.pytest.org/en/stable/how-to/capture-stdout-stderr.html#accessing-captured-output-from-a-test-function
+def test_tyme(capsys):
+    """
+    Tests the tyme function output.
+    """
+    result = tyme(sleep_func)
+    result(1)
+
+    captured = capsys.readouterr()
+    assert captured.out == "Function sleep_func(1,) Took 1.0 seconds\n"
