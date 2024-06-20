cc1aabc15f47586753a33ece3042c811bef0e76d
nurgalive <github@nurgalive.com>
2024-06-20 21:16:32 +0200

    Removed unnecessary round call

--- src/nurgapy/tyme.py
+++ src/nurgapy/tyme.py
@@ -5,7 +5,7 @@ def tyme(some_function):
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
         result = some_function(*args, **kwargs)
-        total_time = round(time.perf_counter() - start_time, 2)
+        total_time = time.perf_counter() - start_time
         print(f"Function {some_function.__name__}{args} Took {total_time:.2f} seconds")
         return result
 
